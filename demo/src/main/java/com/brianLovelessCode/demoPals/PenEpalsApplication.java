c44859e25db76043c5d81df7211d030432729863
Brian Loveless <Brian.loveless1@yahoo.com>
2020-06-23 19:56:36 -0400

    outlines

---
+++ demo/src/main/java/com/brianLovelessCode/demoPals/PenEpalsApplication.java
@@ -0,0 +1,13 @@
+package com.brianLovelessCode.demoPals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenEpalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenEpalsApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/brianLovelessCode/demoPals/PenEpalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianLovelessCode.demoPals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenEpalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
